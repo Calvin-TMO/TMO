2ee4ffed550b2bdccfd73599f611363c20151112
Mark Vander Stel <mvndrstl@gmail.com>
2015-11-16 15:10:08 -0500

    Added first iteration of schema

---
+++ backend/schema.sql
@@ -0,0 +1,73 @@
+
+DROP TABLE Report;
+DROP TABLE CourseStudent;
+DROP TABLE Section;
+DROP TABLE CourseTutor;
+DROP TABLE Course;
+Drop TABLE Professor;
+DROP TABLE Student;
+
+
+CREATE TABLE Student (
+    id integer,
+    fname varchar(70) NOT NULL,
+    lname varchar(70) NOT NULL,
+    email varchar(16) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Professor (
+    id integer,
+    fname varchar(70) NOT NULL,
+    lname varchar(70) NOT NULL,
+    email varchar(16) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Course (
+    id integer,
+    department varchar(5) NOT NULL,
+    num varchar(4) NOT NULL,
+    description varchar(128),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE CourseTutor (
+    courseid integer,
+    tutorid integer,
+    FOREIGN KEY (courseid) REFERENCES Course(id) ON DELETE CASCADE,
+    FOREIGN KEY (tutorid) REFERENCES Student(id) ON DELETE CASCADE
+);
+
+CREATE TABLE Section (
+    id integer,
+    courseid integer,
+    sectionletter varchar(1) NOT NULL,
+    year varchar(4) NOT NULL,
+    semester varchar(2) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (courseid) REFERENCES Course(id) ON DELETE CASCADE
+);
+
+CREATE TABLE CourseStudent (
+    id integer,
+    sectionid integer,
+    studentid integer,
+    tutorid integer,
+    assigneddate date,
+    terminationdate date,
+    PRIMARY KEY (id),
+    FOREIGN KEY (sectionid) REFERENCES Section(id) ON DELETE CASCADE,
+    FOREIGN KEY (studentid) REFERENCES Student(id) ON DELETE CASCADE,
+    FOREIGN KEY (tutorid) REFERENCES Student(id) ON DELETE CASCADE
+);
+
+CREATE TABLE Report (
+    id integer,
+    coursestudentid integer,
+    submitdate date,
+    details varchar(128),
+    PRIMARY KEY (id),
+    FOREIGN KEY (coursestudentid) REFERENCES CourseStudent(id) ON DELETE CASCADE
+);
+
