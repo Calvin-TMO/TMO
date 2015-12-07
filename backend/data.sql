b7113b6fa3f637ffdf587dc04abe4b1b8b626093
Benjamin Braker <bcb9@students.calvin.edu>
2015-12-07 17:49:01 -0500

    Auto-increment starts at 1 so updated inputs.

--- backend/data.sql
+++ backend/data.sql
@@ -8,9 +8,9 @@ INSERT INTO Professor (fname, lname, email) VALUES("Keith", "VanderLinden", "kvl
 INSERT INTO Course (department, num, description) VALUES("CS", "396", "Senior Project I");
 INSERT INTO Course (department, num, description) VALUES("CS", "398", "Senior Project II");
 
-INSERT INTO CourseTutor VALUES(0, 1);
+INSERT INTO CourseTutor VALUES(1, 2);
 
-INSERT INTO CourseStudent (courseid, studentid, tutorid, professorid, assigneddate, terminationdate) VALUES(0, 0, 1, 0, '2015-09-01', '2015-12-18');
+INSERT INTO CourseStudent (courseid, studentid, tutorid, professorid, assigneddate, terminationdate) VALUES(1, 1, 2, 1, '2015-09-01', '2015-12-18');
 
-INSERT INTO Report (coursestudentid, submitdate, starttime, endtime, topic, response, plans, studentplans, comments) VALUES (0, '2015-11-30', '00:00:00', '00:30:00', "Big-O notation", "aggressively", "Study harder", "Get more sleep", NULL);
+INSERT INTO Report (coursestudentid, submitdate, starttime, endtime, topic, response, plans, studentplans, comments) VALUES (1, '2015-11-30', '00:00:00', '00:30:00', "Big-O notation", "aggressively", "Study harder", "Get more sleep", NULL);
 
