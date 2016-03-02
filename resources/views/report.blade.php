58dedbfaa77f6c353bfc80e21762a8c25280458c
Carson Wiens <clw26@15fa-cs394-tmo.cs.calvin.edu>
2016-03-02 21:01:35 +0000

    changed format of assignment and report view

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -4,11 +4,14 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-              <div class="panel-body">
+            <div class="panel panel-default">
+                <div class="panel-heading">Reports</div>
+
+                <div class="panel-body">
                     <p>Coming soon!</p>
-              </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
 @endsection
-
