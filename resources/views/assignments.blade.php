4fd20a48b17f2ab24eb50c8ec74cbf922aafb5c2
Benjamin Braker <bcb9@students.calvin.edu>
2016-03-24 04:02:50 +0000

    Limited viewing access of assignments.
    Create separate tables for all, tutor, and professor assignment lists only viewable if the user has that role.
    
    Limited access to view and edit pages if the user has is associated with it.
    
    Limited access to add page to only admins.
    
    TODO: Only admins should be able to access the edit page.
      Redirecting with improper credentials should bring the user to an Access Denied page.

--- resources/views/assignments.blade.php
+++ resources/views/assignments.blade.php
@@ -8,27 +8,69 @@
                 <div class="panel-heading">Assignments</div>
 
                 <div class="panel-body">
-                    <button class="ui button" onclick="location.href='/assignment/add'">New Assignment</button>
-                    <table class="ui sortable selectable celled table">
-                        <thead>
-                            <tr>
-                                <th class="">Tutor</th>
-                                <th class="">Student</th>
-                                <th class="">Course</th>
-                                <th class="">Professor</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($assignments as $assignment)
-                            <tr class="clickable-row" data-href="/assignment/{{ $assignment->id }}">
-                                <td>{{ $assignment->tutor->name }}</td>
-                                <td>{{ $assignment->student->name }}</td>
-                                <td>{{ $assignment->course->department }}-{{ $assignment->course->number }} {{ $assignment->course->description }}</td>
-                                <td>{{ $assignment->professor->name }}</td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                    @if (Auth::user()->hasRole('admin'))
+                        <button class="ui button" onclick="location.href='/assignment/add'">New Assignment</button>
+                        <table class="ui sortable selectable celled table">
+                            <thead>
+                                <tr>
+                                    <th class="">Tutor</th>
+                                    <th class="">Student</th>
+                                    <th class="">Course</th>
+                                    <th class="">Professor</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($assignments as $assignment)
+                                <tr class="clickable-row" data-href="/assignment/{{ $assignment->id }}">
+                                    <td>{{ $assignment->tutor->name }}</td>
+                                    <td>{{ $assignment->student->name }}</td>
+                                    <td>{{ $assignment->course->department }}-{{ $assignment->course->number }} {{ $assignment->course->description }}</td>
+                                    <td>{{ $assignment->professor->name }}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    @endif
+                    @if (Auth::user()->hasRole('tutor'))
+                        <table class="ui sortable selectable celled table">
+                            <thead>
+                                <tr>
+                                    <th class="">Student</th>
+                                    <th class="">Course</th>
+                                    <th class="">Professor</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($tutor_assignments as $assignment)
+                                <tr class="clickable-row" data-href="/assignment/{{ $assignment->id }}">
+                                    <td>{{ $assignment->student->name }}</td>
+                                    <td>{{ $assignment->course->department }}-{{ $assignment->course->number }} {{ $assignment->course->description }}</td>
+                                    <td>{{ $assignment->professor->name }}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    @endif
+                    @if (Auth::user()->hasRole('professor'))
+                        <table class="ui sortable selectable celled table">
+                            <thead>
+                                <tr>
+                                    <th class="">Tutor</th>
+                                    <th class="">Student</th>
+                                    <th class="">Course</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($professor_assignments as $assignment)
+                                <tr class="clickable-row" data-href="/assignment/{{ $assignment->id }}">
+                                    <td>{{ $assignment->tutor->name }}</td>
+                                    <td>{{ $assignment->student->name }}</td>
+                                    <td>{{ $assignment->course->department }}-{{ $assignment->course->number }} {{ $assignment->course->description }}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    @endif
                 </div>
             </div>
         </div>
