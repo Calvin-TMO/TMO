8d31b9247e2b6b21156697fb58824b77c0ca63ea
Carson Wiens <clw26@tmo.rycieos.com>
2016-04-07 06:20:17 +0000

    attempting to fix non-object

--- database/seeds/AssignmentsTableSeeder.php
+++ database/seeds/AssignmentsTableSeeder.php
@@ -23,24 +23,6 @@ class AssignmentsTableSeeder extends Seeder
             'student_id' => $student->id
         ]);
 
-        $professor = User::where('name', '=', 'professor2')->first();
-        $student = User::where('name', '=', 'student2')->first();
-        DB::table('assignments')->insert([
-            'course_id' => '2',
-            'professor_id' => $professor->id,
-            'tutor_id' => $tutor->id,
-            'student_id' => $student->id
-        ]);
-        
-        $professor = User::where('name', '=', 'Keith VanderLinden')->first();
-        $student = User::where('name', '=', 'Mark VanderStel')->first();
-        DB::table('assignments')->insert([
-            'course_id' => '5',
-            'professor_id' => $professor->id,
-            'tutor_id' => $tutor->id,
-            'student_id' => $student->id
-        ]);
-
         $professor = User::where('name', '=', 'Ruthann Sublett')->first();
         $tutor = User::where('name', '=', 'Heather Monier')->first();
         $student = User::where('name', '=', 'Kaye Stager')->first();
