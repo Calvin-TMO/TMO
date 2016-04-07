3426c7ff5ec6d300e2b27196ce47085c4885b6d0
Carson Wiens <clw26@tmo.rycieos.com>
2016-04-07 05:18:51 +0000

    added the first test data

--- database/seeds/AssignmentsTableSeeder.php
+++ database/seeds/AssignmentsTableSeeder.php
@@ -12,9 +12,9 @@ class AssignmentsTableSeeder extends Seeder
      */
     public function run()
     {
-        $professor = User::where('name', '=', 'professor1')->first();
-        $tutor = User::where('name', '=', 'tutor1')->first();
-        $student = User::where('name', '=', 'student1')->first();
+        $professor = User::where('name', '=', 'Ruthann Sublett')->first();
+        $tutor = User::where('name', '=', 'Heather Monier')->first();
+        $student = User::where('name', '=', 'Suk Percell')->first();
 
         DB::table('assignments')->insert([
             'course_id' => '1',
@@ -36,6 +36,50 @@ class AssignmentsTableSeeder extends Seeder
         $student = User::where('name', '=', 'Mark VanderStel')->first();
         DB::table('assignments')->insert([
             'course_id' => '5',
+            'professor_id' => $professor->id,
+            'tutor_id' => $tutor->id,
+            'student_id' => $student->id
+        ]);
+
+        $professor = User::where('name', '=', 'Ruthann Sublett')->first();
+        $tutor = User::where('name', '=', 'Heather Monier')->first();
+        $student = User::where('name', '=', 'Kaye Stager')->first();
+
+        DB::table('assignments')->insert([
+            'course_id' => '1',
+            'professor_id' => $professor->id,
+            'tutor_id' => $tutor->id,
+            'student_id' => $student->id
+        ]);
+
+        $professor = User::where('name', '=', 'Ruthann Sublett')->first();
+        $tutor = User::where('name', '=', 'Heather Monier')->first();
+        $student = User::where('name', '=', 'Darryl Campa')->first();
+
+        DB::table('assignments')->insert([
+            'course_id' => '1',
+            'professor_id' => $professor->id,
+            'tutor_id' => $tutor->id,
+            'student_id' => $student->id
+        ]);
+
+        $professor = User::where('name', '=', 'Ruthann Sublett')->first();
+        $tutor = User::where('name', '=', 'Heather Monier')->first();
+        $student = User::where('name', '=', 'Carolin Koepsell')->first();
+
+        DB::table('assignments')->insert([
+            'course_id' => '1',
+            'professor_id' => $professor->id,
+            'tutor_id' => $tutor->id,
+            'student_id' => $student->id
+        ]);
+
+        $professor = User::where('name', '=', 'Ruthann Sublett')->first();
+        $tutor = User::where('name', '=', 'Heather Monier')->first();
+        $student = User::where('name', '=', 'Loura Yagi')->first();
+
+        DB::table('assignments')->insert([
+            'course_id' => '1',
             'professor_id' => $professor->id,
             'tutor_id' => $tutor->id,
             'student_id' => $student->id
