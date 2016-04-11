76f70ec380a5e7d5cf432f5abb5ec64265e9b418
Carson Wiens <clw26@tmo.rycieos.com>
2016-04-11 20:41:37 +0000

    hopefully fixed seeding and time increment between session_start and session_end

--- database/seeds/ReportsTableSeeder.php
+++ database/seeds/ReportsTableSeeder.php
@@ -13,17 +13,17 @@ class ReportsTableSeeder extends Seeder
     public function run()
     {
         $topics = ['Programming in C++', 'Giving an Effective Presentation (Even with PowerPoint)', 'Taxes', 'Lethargy'];
-        $date = DateTime::createFromFormat('Y-m-d g:ia', "2016-1-1 08:00am");
+        $date = DateTime::createFromFormat('Y-m-d H:i:s', "2015-1-1 08:00:00");
         for ($i = 1; $i <= 201; $i++) {
             $date = strtotime("+1 day", $date);
             for ($j = 1; $j <= 4; $j++) {
-                $here_date = strtotime("+" . j . " weeks", $date);
+                $here_date = strtotime("+" . $j . " weeks", $date);
                 DB::table('reports')->insert([
                     'session_date' => $here_date,
                     'session_start' => $here_date,
-                    'session_end' => strtotime("+1 hour", $here_date),
+                    'session_end' => strtotime($here_date)+60*60,
                     'assignment_id' => '$i',
-                    'topic' => $topics[j],
+                    'topic' => $topics[$j],
                     'response' => 'Bacon ipsum dolor amet ham hock swine beef ribs, fatback ham andouille pig tenderloin. Porchetta kielbasa ham rump. Prosciutto jerky andouille ball tip, pork belly short loin alcatra bresaola doner tri-tip shoulder turducken. Tri-tip kielbasa pastrami landjaeger leberkas. Alcatra pork leberkas drumstick cow fatback chuck kielbasa swine tail ball tip ham brisket',
                     'plans' => 'Turkey meatloaf hamburger, shankle short ribs swine andouille doner tri-tip sausage chuck pancetta short loin pig frankfurter. Spare ribs leberkas pastrami alcatra tongue fatback.'
                 ]);
